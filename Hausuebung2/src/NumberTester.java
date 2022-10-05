ab6527c37b73c340bf8bae47d788e14a791295e9
OrtnerK192014 <OrtnerK190214@sus.htl-grieskirchen.at>
2022-10-05 11:39:51 +0200

    Anfang Beispiel 3 hinzugefuegt

---
+++ Hausuebung2/src/NumberTester.java
@@ -0,0 +1,24 @@
+public class NumberTester {
+
+    private String fileName;
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setOddEvenTester(NumberTester oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTester palindromeTester) {
+        this.PalindromeTester() = palindromeTester;
+    }
+
+    public void testFile() {
+        
+    }
+}
