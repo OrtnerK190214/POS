d00459cfe15452333cb4916ab36cfecda0303930
OrtnerK192014 <OrtnerK190214@sus.htl-grieskirchen.at>
2022-10-05 11:31:06 +0200

    Beispiel 2 hinzugefuegt

---
+++ Hausuebung2/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Test1");
+        list.add("Test2");
+        list.add("Test3");
+        list.add("Test4");
+
+        Consumer<String> consumer = (String s) -> {
+            System.out.println(s);
+        };
+
+        list.forEach((String s) -> System.out.println(s));
+
+        list.forEach(consumer);
+
+        list.forEach(System.out::print);
+    }
+}
