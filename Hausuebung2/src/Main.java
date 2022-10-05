317b5380571c01a885f4cb74118297741cbbba38
OrtnerK192014 <OrtnerK190214@sus.htl-grieskirchen.at>
2022-10-05 19:51:48 +0200

    Benuzterschnittstelle

--- Hausuebung2/src/Main.java
+++ Hausuebung2/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class Main {
@@ -28,6 +29,35 @@ public class Main {
 
         //Beispiel 4
 
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        int eingabe = 0;
+        do {
+            System.out.println("Choose calculator: ");
+            System.out.printf("1 - Relational calculator");
+            System.out.printf("2 - Vector calculator");
+            System.out.printf("3 - Complex calculator");
+            System.out.printf("4 - Exit program");
+            eingabe = scanner.nextInt();
 
+            System.out.println("Enter number x a> 1");
+            int a1 = scanner.nextInt();
+            System.out.println("Enter number x b> 1");
+            int a2 = scanner.nextInt();
+            System.out.println("Enter number y a> 1");
+            int b1 = scanner.nextInt();
+            System.out.println("Enter number y b> 1");
+            int b2 = scanner.nextInt();
+            Number number1 = new Number(a1, a2);
+            Number number2 = new Number(b1, b2);
+
+            System.out.println("Choose operation: ");
+            System.out.println("1 - add");
+            System.out.println("2 - subtract");
+            System.out.println("3 - multiply");
+            System.out.println("4 - divide");
+            System.out.println("5 - enter numbers again");
+            int eingabe2 = scanner.nextInt();
+        } while (eingabe != 4);
     }
 }
