c67f401b82aba2fcd1eb395e3af7c615bc082c53
OrtnerK192014 <OrtnerK190214@sus.htl-grieskirchen.at>
2022-10-12 17:26:40 +0200

    Hausuebung 3: Comparator hinzugefuegt

--- Hausuebung3/src/Main.java
+++ Hausuebung3/src/Main.java
@@ -1,5 +1,52 @@
+import java.util.*;
+
 public class Main {
+
+    private List<Weapon> weaponslist = new ArrayList<>();
+
+    private Comparator<Weapon> absteigendamage = new Comparator<Weapon>() {
+        @Override
+        public int compare(Weapon o1, Weapon o2) {
+            return o1.getDamage() - o2.getDamage();
+        }
+    };
+
+    private Comparator<Weapon> alphabetisch = new Comparator<Weapon>() {
+        @Override
+        public int compare(Weapon o1, Weapon o2) {
+            int i1 = o1.getCombatType().compareTo(o2.getCombatType());
+            int i2 = -1;
+            if (i1 == 0)
+                i2 = o1.getDamageType().compareTo(o2.getDamageType());
+            if (i2 == 0)
+                return o1.getName().compareTo(o2.getName());
+            return i1;
+        }
+    };
+
+    private Printable printable = new Printable() {
+        @Override
+        public void print(List<Weapon> weapons) {
+            for (int i = 0; i<weapons.size(); i++)
+            {
+                System.out.println(weapons.get(i));
+            }
+        }
+    };
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public List<Weapon> sortdamage(List<Weapon> weapons)
+    {
+        Collections.sort(weapons, absteigendamage);
+        return weapons;
+    }
+
+    public List<Weapon> sortalphabetisch(List<Weapon> weapons)
+    {
+        Collections.sort(weapons, alphabetisch);
+        return weapons;
     }
 }
--- Hausuebung3/src/Weapon.java
+++ Hausuebung3/src/Weapon.java
@@ -17,6 +17,19 @@ public class Weapon {
         this.value = value;
     }
 
+    @Override
+    public String toString() {
+        return "Weapon{" +
+                "name='" + name + '\'' +
+                ", combatType=" + combatType +
+                ", damageType=" + damageType +
+                ", damage=" + damage +
+                ", speed=" + speed +
+                ", strength=" + strength +
+                ", value=" + value +
+                '}';
+    }
+
     public String getName() {
         return name;
     }
